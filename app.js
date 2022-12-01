db61bdda1f2516b6298312cba54f9893e49a73ed
seviLT <98219835+seviLT@users.noreply.github.com>
2022-12-01 18:14:25 +0000

    3. operate function; operator and 2 nums

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //add two numbers
 function add(num1,num2)
 {
+    console.log("addition ", num1 + num2)
     return num1 + num2;
 }
 
@@ -8,6 +9,7 @@ console.log(add(2,3))
 
 function subtract(num1,num2)
 {
+    
     return num1 - num2;
 }
 
@@ -26,6 +28,37 @@ function divide(num1,num2)
     return num1 / num2;
 }
 
-console.log(add(2,3))
+
 console.log(divide(2,1))
 console.log(divide(2,0))
+
+function operate(operator, num1, num2)
+{
+    //operators ->  + , - , * , /
+
+    switch(operator)
+    {
+        case '+':
+            add(num1,num2);
+            break;
+
+        case '-':
+             subtract(num1,num2);
+             break;
+
+        case '*':
+             multiply(num1,num2);
+             break;
+
+        case '/':
+             divide(num1,num2);
+             break;
+
+        default: 
+            'wrong operator';     
+
+    }
+}
+
+operate('+',7,3);
+operate('+',3,3);
